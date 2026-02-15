178e2fea18d66df46fcd54ad239337edc87a1ea2
healthboostcentral2-web <healthboostcentral2@gmail.com>
2026-02-16 00:44:42 +0530

    Add base path for Netlify deployment
    Added base configuration for Netlify deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
+  base: "./",   // ⭐⭐⭐ এটা না থাকলে Netlify blank page দেয়
 })
